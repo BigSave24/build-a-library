cff46b0ef1f63df30902524bee6c6fc79160832f
Kedmond24 <Kedmond24@live.com>
2021-09-06 20:44:08 -0400

    ADD media class properties

--- media.js
+++ media.js
@@ -1,6 +1,50 @@
 // Parent Class
 class Media {
-    constructor(title, ratings){
-        // Complete
+    constructor(title){
+        this._title = title;
+        this._ratings = [];
+        this._isCheckedOut = false;
+    }
+
+    get title(){
+        return this._title
+    }
+
+    // Ratings Methods
+    get ratings(){
+        return this._ratings
+    }
+
+    getAverageRating(){
+        let ratingsCount = this._ratings.length
+        let totalRatings = this._ratings.reduce((acc, curr) => acc + curr)
+
+        return totalRatings/ratingsCount
+    }
+
+    addRating(newRating){
+        this._ratings.push(newRating)
+    }
+
+    // Checkout Methods
+    get isCheckedOut(){
+        return this._isCheckedOut
+    }
+
+    toggleCheckedOutStatus(setCheckoutStatus){
+        this._isCheckedOut = setCheckoutStatus
     }
 }
+
+
+
+// class Book 
+// author pages 
+
+// class Movie 
+// director runTime 
+
+// class CD
+// artist songs 
+
+
